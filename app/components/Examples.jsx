0bcafc4c24a078b56e3bac691ed244737da579af
Neil Yeats <neilyeats@Neils-iMac.local>
2016-08-30 20:31:57 +1200

    updated examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,9 +2,8 @@ var React = require('react');
 
 var Examples = (props) => {
     return (<div>
-        <h2>
-            Examples</h2>
-          <p>Welcome to examples</p>
+        <h2>Examples</h2>
+          <p>Welcome to examples!</p>
           </div>
     )
 };
