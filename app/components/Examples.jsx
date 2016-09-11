53703ef5a37c4d59a70d425caf2fc8a155ee2423
Neil Yeats <neilyeats@Neils-iMac.local>
2016-09-11 23:19:03 +1200

    add foundation & jquery

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Examples = (props) => {
     return (<div>
         <h2>Examples</h2>
-          <p>Welcome to examples !!!</p>
+          <p>Welcome to examples  - 123</p>
           </div>
     )
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 
 var LiveReloadPlugin = require('webpack-livereload-plugin');
 
+var webpack=require('webpack');
 module.exports = {
-  entry:'./app/app.jsx',
+  entry:[
+'script!jquery/dist/jquery.min.js',
+'script!foundation-sites/dist/foundation.min.js',
+'./app/app.jsx'
+],
+externals:{
+  jquery: 'jQuery'
+},
+plugins:[
+  new webpack.ProvidePlugin({
+    '$':'jquery',
+    'jQuery':'jquery'
+  })
+],
   output:{
     path: __dirname,
     filename:'./public/bundle.js'
   },
     plugins: [
       new LiveReloadPlugin()
-    ]
-  ,
+    ],
   resolve:{
     root: __dirname,
     alias:{
